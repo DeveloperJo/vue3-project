774ad22c9133762bf45ab1e231d919474765bb1b
Jugab <97277282+Jugab@users.noreply.github.com>
2023-02-14 17:49:51 +0900

    v-model을 활용한 양방향 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<input type="text" :value="name" @input="updateName" />
+		<input type="text" v-model="name" />
 		<button class="btn btn-primary" @click="onSubmit">Click</button>
 	</div>
 </template>
@@ -13,17 +13,11 @@ export default {
 		const name = ref('Worksheets 1'); // ref는 string, int, object, list 모든 타입을 사용할 수 있다. 값을 치환할 때는 value를 이용해야 한다.
 
 		const onSubmit = () => {
-			console.log(name);
-		};
-
-		const updateName = (e) => {
-			//console.log('updateName : ' + name.value);
-			console.log(e);
+			console.log(name.value);
 		};
 
 		return {
 			name,
-			updateName,
 			onSubmit,
 		};
 	},
