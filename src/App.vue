e60c79c20e6580f55c67fb185feb99141a6659c1
Jugab <97277282+Jugab@users.noreply.github.com>
2023-02-14 18:04:23 +0900

    To-Do 리스트 만들기 1 - input 입력 전송

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-	<div>
-		<input type="text" v-model="name" />
-		<button class="btn btn-primary" @click="onSubmit">Click</button>
+	<div class="container">
+		<h2>To-Do List</h2>
+		<form class="d-flex" @submit.prevent="onSubmit">
+			<div class="flex-grow-1 mr-2">
+				<input
+					class="form-control"
+					type="text"
+					v-model="todo"
+					placeholder="Type new To-Do"
+				/>
+			</div>
+			<div>
+				<button type="submit" class="btn btn-primary">Click</button>
+			</div>
+		</form>
+		{{ todos }}
 	</div>
 </template>
 
@@ -10,14 +23,20 @@ import { ref } from 'vue';
 
 export default {
 	setup() {
-		const name = ref('Worksheets 1'); // ref는 string, int, object, list 모든 타입을 사용할 수 있다. 값을 치환할 때는 value를 이용해야 한다.
+		const todo = ref(''); // ref는 string, int, object, list 모든 타입을 사용할 수 있다. 값을 치환할 때는 value를 이용해야 한다.
+		const todos = ref([]);
 
 		const onSubmit = () => {
-			console.log(name.value);
+			console.log(todo.value);
+			todos.value.push({
+				id: Date.now(),
+				subject: todo.value,
+			});
 		};
 
 		return {
-			name,
+			todo,
+			todos,
 			onSubmit,
 		};
 	},
