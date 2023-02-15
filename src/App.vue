ba6b10950a594c5f2b0802fefd5093e14a8ca4a5
Jugab <97277282+Jugab@users.noreply.github.com>
2023-02-15 14:56:00 +0900

    검색 기능 추가, computed를 통해 filter함수 실행하여 todo 리스트 변화

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
 	<div class="container">
-		<h4>count : {{ count }}</h4>
-		<h4>double count computed : {{ doubleCountComputed }}</h4>
-		<h4>double count method : {{ doubleCountMethod() }}</h4>
-		<button @click="count++">Add one</button>
 		<h2>To-Do List</h2>
+		<input
+			class="form-control"
+			type="text"
+			v-model="searchText"
+			placeholder="Search"
+		/>
+		<hr />
 		<TodoSimpleForm @add-todo="addTodo" />
-		<div v-if="!todos.length">추가된 To-Do가 없습니다.</div>
+		<div v-if="!filteredTodos.length">추가된 To-Do가 없습니다.</div>
 		<TodoList
-			:todos="todos"
+			:todos="filteredTodos"
 			@toggle-todo="toggleTodo"
 			@delete-todo="deleteTodo"
 		/>
@@ -46,18 +49,16 @@ export default {
 			todos.value.splice(index, 1);
 		};
 
-		const count = ref(1);
-		// computed vs method
-		// 1) method는 인자값을 받아서 처리가 가능. computed는 불가능
-		// 2) computed는 reactive되는 값이 있어야 한다.
-		// 3) computed는 캐시에 저장이 되어, 한번 계산을 하고 값을 저장을 하고 있다. 두번 연달아 사용하는 경우 한번만 호출한다
-		const doubleCountComputed = computed(() => {
-			return count.value * 2;
-		});
+		const searchText = ref('');
+		const filteredTodos = computed(() => {
+			if (searchText.value) {
+				return todos.value.filter((todo) => {
+					return todo.subject.includes(searchText.value);
+				});
+			}
 
-		const doubleCountMethod = () => {
-			return count.value * 2;
-		};
+			return todos.value;
+		});
 
 		return {
 			todos,
@@ -65,9 +66,8 @@ export default {
 			addTodo,
 			toggleTodo,
 			deleteTodo,
-			count,
-			doubleCountComputed,
-			doubleCountMethod,
+			searchText,
+			filteredTodos,
 		};
 	},
 };
